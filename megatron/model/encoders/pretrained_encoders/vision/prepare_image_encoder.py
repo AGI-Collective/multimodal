81793388f30bf05da543ee9c8b8688f32d5bbdb1
daniel-z-kaplan <48258016+daniel-z-kaplan@users.noreply.github.com>
2023-09-05 15:18:43 -0400

    Added dino pretrained model download

--- megatron/model/encoders/pretrained_encoders/vision/prepare_image_encoder.py
+++ megatron/model/encoders/pretrained_encoders/vision/prepare_image_encoder.py
@@ -11,3 +11,21 @@ image_prefix = ImageEncoder(
     out_dim=neox_args.hidden_size,
 )
 print(f"Downloaded pretrain weight of {neox_args.encoder_name} to {neox_args.load} !")
+
+
+
+def download_dino(size = "base", location):
+
+    import torch
+    BACKBONE_SIZE = size # in ("small", "base", "large" or "giant")
+    backbone_archs = {
+        "small": "vits14",
+        "base": "vitb14",
+        "large": "vitl14",
+        "giant": "vitg14",
+    }
+    backbone_arch = backbone_archs[BACKBONE_SIZE]
+    backbone_name = f"dinov2_{backbone_arch}"
+    
+    backbone_model = torch.hub.load(repo_or_dir="facebookresearch/dinov2", model=backbone_name)
+    torch.save(backbone_model.state_dict(), str(location) + "./dino"+str(size)+".pt")
