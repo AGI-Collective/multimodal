bc92f3f815fdd1f2e0fa9c47939df5abf3d8d329
floatingsnake <1227073631@qq.com>
2023-09-06 15:45:26 +0900

    reconstruct encoder

--- mytests/prepare_image_encoder.py
+++ mytests/prepare_image_encoder.py
@@ -5,17 +5,7 @@ from megatron.model.encoders.image_encoders import ImageEncoder
 from megatron.neox_arguments import NeoXArgs
 
 
-neox_args = NeoXArgs.from_ymls(['./configs/summit-70m-openclipH.yml', './configs/summit_setup.yml'])
-image_prefix = ImageEncoder(
-    config = neox_args,
-    out_dim=neox_args.hidden_size,
-)
-print(f"Downloaded pretrain weight of {neox_args.encoder_name} to {neox_args.load} !")
-
-
-
 def download_dino(size = "base", location):
-
     import torch
     BACKBONE_SIZE = size # in ("small", "base", "large" or "giant")
     backbone_archs = {
