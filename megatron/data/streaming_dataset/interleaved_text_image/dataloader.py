Fixed masking, refactored code

megatron/data/streaming_dataset/interleaved_text_image/dataloader.py
@@ -47,7 +47,7 @@ class StreamingInterleavedDataset(StreamingDataset):
     Args:
         tokenizer (Tokenizer): HuggingFace tokenizer to
             tokenize samples.
-        max_seq_len (int): The max sequence length of each sample.
+        max_seq_length (int): The max sequence length of each sample.
         streams (Sequence[Stream], optional): One or more Streams to stream/cache samples from,
             which may be upsampled or downsampled. StreamingDataset uses either ``streams`` or
             ``remote``/``local``. Defaults to ``None``.
@@ -93,7 +93,7 @@ class StreamingInterleavedDataset(StreamingDataset):
 
     def __init__(self,
                  tokenizer: PreTrainedTokenizerBase,
-                 max_seq_len: int,
+                 max_seq_length: int,
                  streams: Optional[Sequence[Stream]] = None,
                  remote: Optional[str] = None,
                  local: Optional[str] = None,
@@ -156,7 +156,7 @@ class StreamingInterleavedDataset(StreamingDataset):
             shuffle_block_size=shuffle_block_size,
         )
         self.tokenizer = tokenizer
-        self.max_seq_len = max_seq_len
+        self.max_seq_length = max_seq_length
 
     # How to tokenize a text sample to a token sample
     def _tokenize(self, text_sample: Mapping):
@@ -168,12 +168,12 @@ class StreamingInterleavedDataset(StreamingDataset):
         return self.tokenizer(text_sample['text'],
                               truncation=True,
                               padding='max_length',
-                              max_length=self.max_seq_len)
+                              max_length=self.max_seq_length)
 
     def _read_binary_tokenized_sample(self, sample: Dict[str, Any]):
         return torch.from_numpy(
             np.frombuffer(sample['tokens'],
-                          dtype=np.int64)[:self.max_seq_len].copy())
+                          dtype=np.int64)[:self.max_seq_length].copy())
     
     def _read_binary_data(self, sample):
         return torch.from_numpy(
@@ -191,93 +191,32 @@ class StreamingInterleavedDataset(StreamingDataset):
             raise RuntimeError(
                 'StreamingTextDataset needs samples to have a `text` or `tokens` column'
             )
-        images = self._read_binary_data(sample.get('images', None))
+        vision_input = torch.from_numpy(sample.get('vision_input', None).copy())
         multimodal_position_ids = torch.from_numpy(sample.get('multimodal_position_ids', None).copy())
-        # return {
-        #     'input_ids': token_sample,
-        #     'image': sample['image'],
-        #     'position_ids': sample['position_ids'],
-        # }
-
-        return (token_sample, images, multimodal_position_ids)
+        labels = torch.from_numpy(sample.get('labels', None).copy()) 
+        return (token_sample, vision_input, multimodal_position_ids, labels)
 
 
 # Multimodal Collate Function
 class MultimodalCollateWrapper:
-    def __init__(self, text_collator, image_collator, video_collator, audio_collator, multimodal_position_ids_collator) -> None:
+    def __init__(self, text_collator, vision_collator, video_collator, audio_collator, multimodal_position_ids_collator, label_collator) -> None:
         self.text_collator = text_collator
-        self.image_collator = image_collator
+        self.vision_collator = vision_collator
         # self.video_collator = video_collator
         # self.audio_collator = audio_collator
         self.multimodal_position_ids_collator = multimodal_position_ids_collator
+        self.label_collator = label_collator
     
     def __call__(self, examples: List[Any]) -> Dict[str, torch.Tensor]:
         # Convert examples (list of tuples) to a list of lists (one list per modality)
         examples = list(zip(*examples))
         batch = self.text_collator(examples[0])
-        batch['images'] = self.image_collator(examples[1])
+        batch['vision_input'] = self.vision_collator(examples[1])
         # batch['video'] = self.video_collator(examples[2])
         # batch['audio'] = self.audio_collator(examples[3])
         batch['multimodal_position_ids'] = self.multimodal_position_ids_collator(examples[2])
+        batch['labels'] = self.label_collator(examples[3])
         return batch
-
-
-class ConcatenatedSequenceCollatorWrapper:
-    """Collator wrapper to add sequence_id to batch."""
-
-    def __init__(
-        self,
-        base_collator: Callable,
-        eos_token_id: Optional[int] = None,
-        bos_token_id: Optional[int] = None,
-    ):
-        self.base_collator = base_collator
-        if (eos_token_id is None) and (bos_token_id is None):
-            raise ValueError(
-                'Must supply a value for either eos_token_id or bos_token_id, but got None for both.'
-            )
-        if (eos_token_id is not None) and (bos_token_id is not None):
-            raise ValueError(
-                'Cannot use *both* EOS and BOS tokens for detecting sequence boundaries. ' +\
-                'Please supply `eos_token_id` if sequences end with an EOS token, or use ' +\
-                '`bos_token_id` if sequences start with a BOS token.'
-            )
-
-        self.split_token_id = eos_token_id
-        self.bos_mode = False
-        if eos_token_id is None:
-            self.split_token_id = bos_token_id
-            self.bos_mode = True
-
-    def __call__(self, examples: List[Any]) -> Dict[str, torch.Tensor]:
-        batch = self.base_collator(examples)
-        batch['sequence_id'] = self.get_sequence_id_from_batch(batch)
-        return batch
-
-    def get_sequence_id_from_batch(
-            self, batch: Dict[str, torch.Tensor]) -> torch.Tensor:
-        
-        multimodal_position_ids = batch['multimodal_position_ids'] # TODO
-        number_of_modalities = multimodal_position_ids.shape[0]
-        batch_size = multimodal_position_ids.shape[1]
-        max_seq_len = multimodal_position_ids.shape[2]
-        tokens = torch.zeros((batch_size, max_seq_len), dtype=torch.long, device=multimodal_position_ids.device)
-        token_ids = batch['input_ids']
-        
-        # tokens[multimodal_position_ids[:, 0, :]] = token_ids
-        tokens[torch.arange(tokens.size(0))[:,None], multimodal_position_ids[:, 0, :]] = token_ids
-        is_separator = torch.eq(tokens,
-                                self.split_token_id)  # type: ignore
-        cumulative_sep = torch.cumsum(is_separator,
-                                      dim=1).to(batch['input_ids'].dtype)
-        # If separator token is bos, we're already done
-        if self.bos_mode:
-            return cumulative_sep
-
-        # If separator token is eos, right shift 1 space
-        left_zeros = cumulative_sep.new_zeros((cumulative_sep.shape[0], 1))
-        return torch.cat([left_zeros, cumulative_sep[:, :-1]], dim=1)
-
 
 class TextNeoXCollateWrapper:
 
@@ -286,7 +225,7 @@ class TextNeoXCollateWrapper:
     
     def __call__(self, examples: List[Any]) -> Dict[str, torch.Tensor]:
         batch = self.collator(examples)
-        batch['text'] = batch['input_ids']
+        batch['text_input'] = batch['input_ids'] # TODO Delete input_ids to save comm overhead and space
         return batch
 
 class PaddedCollateWrapper:
@@ -307,7 +246,7 @@ class PaddedCollateWrapper:
     
 def build_interleaved_dataloader(
     cfg: DictConfig,
-    tokenizer: PreTrainedTokenizerBase,
+    tokenizer,
     device_batch_size: int,
 ):
     assert cfg.name == 'text', f'Tried to build text dataloader with cfg.name={cfg.name}'
@@ -347,23 +286,19 @@ def build_interleaved_dataloader(
 
     text_collate_fn = TextNeoXCollateWrapper(text_collate_fn)
     
-    image_collate_fn = PaddedCollateWrapper(pad_token_id=-1) # Each sample: (num_images, H, W, C)
+    vision_collate_fn = PaddedCollateWrapper(pad_token_id=-1) # Each sample: (num_vision, H, W, C)
 
-    multimodal_position_ids_collate_fn = PaddedCollateWrapper(pad_token_id=-1, take_transpose=True) # Each sample: (num_modalities, max_seq_len)
+    multimodal_position_ids_collate_fn = PaddedCollateWrapper(pad_token_id=-1, take_transpose=True) # Each sample: (num_modalities, max_seq_length)
     
+    label_collate_fn = PaddedCollateWrapper(pad_token_id=-1) # Each sample: (num_modalities, max_seq_length)
+
     collate_fn = MultimodalCollateWrapper(text_collator=text_collate_fn, 
-                                          image_collator=image_collate_fn, 
+                                          vision_collator=vision_collate_fn, 
                                           video_collator=None, 
                                           audio_collator=None, 
-                                          multimodal_position_ids_collator=multimodal_position_ids_collate_fn)
-
-    if (eos_token_id is not None) or (bos_token_id is not None):
-        # Note: Will raise an error if both are non-None
-        collate_fn = ConcatenatedSequenceCollatorWrapper(
-            base_collator=collate_fn,
-            eos_token_id=eos_token_id,
-            bos_token_id=bos_token_id)
-
+                                          multimodal_position_ids_collator=multimodal_position_ids_collate_fn, 
+                                          label_collator=label_collate_fn)
+    
     return DataLoader(
         dataset,
         collate_fn=collate_fn,
@@ -399,7 +334,7 @@ if __name__ == '__main__':
                         type=str,
                         default='val',
                         help='which split of the dataset to use')
-    parser.add_argument('--max_seq_len',
+    parser.add_argument('--max_seq_length',
                         type=int,
                         default=32,
                         help='max sequence length to test')
@@ -420,7 +355,7 @@ if __name__ == '__main__':
             'remote': args.remote_path,
             'split': args.split,
             'shuffle': False,
-            'max_seq_len': args.max_seq_len,
+            'max_seq_length': args.max_seq_length,
             'keep_zip': True,  # in case we need compressed files after testing
         },
         'drop_last': False,
@@ -430,11 +365,11 @@ if __name__ == '__main__':
     device_batch_size = 2
 
     tokenizer_cfg = {'name': args.tokenizer, 'kwargs': {}}
-    tokenizer_cfg['kwargs'] = {'model_max_length': args.max_seq_len}
+    tokenizer_cfg['kwargs'] = {'model_max_length': args.max_seq_length}
     tokenizer_cfg = om.create(tokenizer_cfg)
     tokenizer = build_tokenizer(tokenizer_cfg)
 
-    loader = build_text_dataloader(cfg, tokenizer, device_batch_size)
+    loader = build_interleaved_dataloader(cfg, tokenizer, device_batch_size)
     tokenizer = loader.dataset.tokenizer  # type: ignore
     for batch_ix, batch in enumerate(islice(loader, 5)):
         print('\n')

megatron/data/streaming_dataset/text/dataloader.py
@@ -47,7 +47,7 @@ class StreamingTextDataset(StreamingDataset):
     Args:
         tokenizer (Tokenizer): HuggingFace tokenizer to
             tokenize samples.
-        max_seq_len (int): The max sequence length of each sample.
+        max_seq_length (int): The max sequence length of each sample.
         streams (Sequence[Stream], optional): One or more Streams to stream/cache samples from,
             which may be upsampled or downsampled. StreamingDataset uses either ``streams`` or
             ``remote``/``local``. Defaults to ``None``.
@@ -93,7 +93,7 @@ class StreamingTextDataset(StreamingDataset):
 
     def __init__(self,
                  tokenizer: PreTrainedTokenizerBase,
-                 max_seq_len: int,
+                 max_seq_length: int,
                  streams: Optional[Sequence[Stream]] = None,
                  remote: Optional[str] = None,
                  local: Optional[str] = None,
@@ -156,7 +156,7 @@ class StreamingTextDataset(StreamingDataset):
             shuffle_block_size=shuffle_block_size,
         )
         self.tokenizer = tokenizer
-        self.max_seq_len = max_seq_len
+        self.max_seq_length = max_seq_length
 
     # How to tokenize a text sample to a token sample
     def _tokenize(self, text_sample: Mapping):
@@ -168,12 +168,12 @@ class StreamingTextDataset(StreamingDataset):
         return self.tokenizer(text_sample['text'],
                               truncation=True,
                               padding='max_length',
-                              max_length=self.max_seq_len)
+                              max_length=self.max_seq_length)
 
     def _read_binary_tokenized_sample(self, sample: Dict[str, Any]):
         return torch.from_numpy(
             np.frombuffer(sample['tokens'],
-                          dtype=np.int64)[:self.max_seq_len].copy())
+                          dtype=np.int64)[:self.max_seq_length].copy())
 
     # How to process a sample
     def __getitem__(self, idx: int):
@@ -330,7 +330,7 @@ if __name__ == '__main__':
                         type=str,
                         default='val',
                         help='which split of the dataset to use')
-    parser.add_argument('--max_seq_len',
+    parser.add_argument('--max_seq_length',
                         type=int,
                         default=32,
                         help='max sequence length to test')
@@ -351,7 +351,7 @@ if __name__ == '__main__':
             'remote': args.remote_path,
             'split': args.split,
             'shuffle': False,
-            'max_seq_len': args.max_seq_len,
+            'max_seq_length': args.max_seq_length,
             'keep_zip': True,  # in case we need compressed files after testing
         },
         'drop_last': False,
@@ -361,7 +361,7 @@ if __name__ == '__main__':
     device_batch_size = 2
 
     tokenizer_cfg = {'name': args.tokenizer, 'kwargs': {}}
-    tokenizer_cfg['kwargs'] = {'model_max_length': args.max_seq_len}
+    tokenizer_cfg['kwargs'] = {'model_max_length': args.max_seq_length}
     tokenizer_cfg = om.create(tokenizer_cfg)
     tokenizer = build_tokenizer(tokenizer_cfg)
 

megatron/model/word_embeddings.py
@@ -185,9 +185,14 @@ class ImageEncoder(torch.nn.Module):
 
 
     def forward(self, images):
+        # images is [B, T, F, C, H, W]
+        num_images = images.shape[1]
+        original_batch_size = images.shape[0]
+        images = rearrange(images, "b t f c h w -> (b t) f c h w")
+
         batch_size = images.shape[0]
-        max_seq_len = images.shape[1]
-        # images = images.reshape(batch_size, max_seq_len, -1)
+        max_seq_length = images.shape[1]
+        # images = images.reshape(batch_size, max_seq_length, -1)
         # print("Shape of images", images.shape)
 
         # images = images.reshape((images.shape[0] * images.shape[1], images.shape[2], images.shape[3], images.shape[4]))
@@ -203,7 +208,8 @@ class ImageEncoder(torch.nn.Module):
         # images = images.reshape()
         output = output[:, 0, :]
         embeddings = self.encoder_layer(output)
-        embeddings = embeddings.reshape(batch_size, max_seq_len, -1)  
+        embeddings = embeddings.reshape(batch_size, max_seq_length, -1)  
+        embeddings = rearrange(embeddings, "(b t) n f -> b t n f", b=original_batch_size, t=num_images)
         return embeddings
 
 class EmbeddingPipe(Embedding):
@@ -222,18 +228,18 @@ class EmbeddingPipe(Embedding):
     def forward(self, args):
         assert (
             len(args) == 5
-        ), f"Expected 3 arguments (input_ids, images, multimodal_position_ids, position_ids, attention_mask), but got {len(args)}."
+        ), f"Expected 3 arguments (input_ids, vision_input, multimodal_position_ids, position_ids, attention_mask), but got {len(args)}."
 
         input_ids = args[0]
-        images = args[1]
+        vision_input = args[1]
         multimodal_position_ids = args[2] # [B, T]
-        assert self.seq_length == torch.max(multimodal_position_ids)
+        assert self.seq_length == torch.max(multimodal_position_ids)+1
         position_ids = args[3]
         attention_mask = args[4]
         
         word_embeddings = super().forward(input_ids, position_ids) # [B, T, E]
-        
-        image_embeddings = self.image_encoder(images) # [B, T, N, E] where N=1 for now 
+        # Vision Input is [B, T, F, C, H, W]
+        image_embeddings = self.image_encoder(vision_input) # [B, T, N, E] where N=1 for now 
         image_embeddings = rearrange(image_embeddings, "b t n e -> b (t n) e") # [B, T*N, E]
         
         # Concatenate the embeddings
@@ -247,8 +253,9 @@ class EmbeddingPipe(Embedding):
         time_steps = all_embeddings.shape[1]
         batch_indices = torch.arange(batch_size).view(-1, 1).expand(-1, time_steps)
         all_embeddings = all_embeddings[batch_indices, multimodal_position_ids]
-        
-        assert all(multimodal_position_ids[batch_indices, multimodal_position_ids][:, self.seq_length:, :]) == -1, "Multimodal position ids should be -1 after the sequence length"
+        # print(multimodal_position_ids[batch_indices, multimodal_position_ids].shape, multimodal_position_ids[batch_indices, multimodal_position_ids])
+        if multimodal_position_ids.shape[1] > self.seq_length:
+            assert all(multimodal_position_ids[batch_indices, multimodal_position_ids][:, self.seq_length:]) == -1, "Multimodal position ids should be -1 after the sequence length"
         return all_embeddings, attention_mask
 
 

megatron/training.py
@@ -33,8 +33,9 @@ import numpy as np
 from megatron.utils import (
     Timers,
     init_wandb,
-    get_ltor_masks_and_position_ids, get_multimodal_ltor_masks_and_position_ids,
+    get_multimodal_ltor_masks_and_position_ids,
     reduce_losses,
+    MODALITY_DICT
 )
 
 from megatron import print_rank_0, mpu
@@ -276,26 +277,54 @@ def _get_batch(neox_args, tokenizer, keys, data, datatype):
     data_b = mpu.broadcast_data(keys, data, datatype)
 
     # Unpack text.
-    tokens_ = data_b["text"].long()
+    text_input_ = data_b["text_input"].long()
     if "labels" in data_b: # This our custom approach
         labels = data_b["labels"].long().contiguous()
-        tokens = tokens_.contiguous()
+        text_input = text_input_.contiguous()
     else:
-        labels = tokens_[:, 1:].contiguous()
-        tokens = tokens_[:, :-1].contiguous()
-
-    # Unpack images.
-    images = data_b["images"].half().contiguous()
+        labels = text_input_[:, 1:].contiguous()
+        text_input = text_input_[:, :-1].contiguous()
 
     # Unpack multimodal position ids.
     multimodal_position_ids = data_b["multimodal_position_ids"].long().contiguous()
 
+    max_text_length = text_input.shape[1]
+    text_positions = multimodal_position_ids[:, MODALITY_DICT['text'], :max_text_length]
+    text_input_info = {
+        "input": text_input,
+        "positions": text_positions,
+        "seq_length": 1
+    }
+
+    # Unpack vision_input.
+    vision_input = data_b["vision_input"].half().contiguous()
+    max_vision_length = vision_input.shape[1]
+    vision_positions = multimodal_position_ids[:, MODALITY_DICT['vision'], :max_vision_length]
+    vision_input_info = {
+        "input": vision_input,
+        "positions": vision_positions,
+        "seq_length": neox_args.vision_seq_length
+    }
+
+    # max_audio_length = audio_input.shape[1]
+    # audio_positions = multimodal_position_ids[:, MODALITY_DICT['audio'], :max_audio_length]
+    # audio_input_info = {
+    #     "input": audio_input,
+    #     "positions": audio_positions,
+    #     "seq_length": neox_args.audio_seq_length
+    # }
+    audio_input_info = None
+
+    input_info = {
+        "text": text_input_info,
+        "vision": vision_input_info,
+        "audio": audio_input_info
+    }
+
     # Get the masks and position ids.
     attention_mask, loss_mask, position_ids, shifted_multimodal_position_ids = get_multimodal_ltor_masks_and_position_ids(
-        text_tokens = tokens,
+        input_info=input_info,
         labels=labels,
-        multimodal_position_ids=multimodal_position_ids,
-        vision_seq_length=neox_args.vision_seq_length,
         input_seq_length=neox_args.seq_length,
         eod_token=neox_args.tokenizer.eod_id,
         bos_token=neox_args.tokenizer.bos_id if hasattr(neox_args.tokenizer, "bos_id") else None,
@@ -304,14 +333,14 @@ def _get_batch(neox_args, tokenizer, keys, data, datatype):
     )
 
     multimodal_position_ids = shifted_multimodal_position_ids # The shifted corrected version is directly used    
-    return tokens, images, multimodal_position_ids, labels, loss_mask, attention_mask, position_ids
+    return text_input, vision_input, multimodal_position_ids, labels, loss_mask, attention_mask, position_ids
 
 
 def get_batch(neox_args, data_iterator):
     """Generate a batch"""
 
     # Items and their type.
-    keys =  ["text", "images", "multimodal_position_ids", "labels"]
+    keys =  ["text_input", "vision_input", "multimodal_position_ids", "labels"]
 
     datatype = torch.int64
 
@@ -332,19 +361,14 @@ def get_batch(neox_args, data_iterator):
 def get_batch_pipe(data, neox_args, curr_scheduler=None):
     """A modification of get_batch() to work with the latest batch instead of an iterator."""
     # Items and their type.
-    keys =  ["text", "images", "multimodal_position_ids", "labels"]    
+    keys =  ["text_input", "vision_input", "multimodal_position_ids", "labels"]    
     datatype = torch.int64
 
-    tokens, images, multimodal_position_ids, labels, loss_mask, attention_mask, position_ids = _get_batch(
+    # Text Input is renamed to tokens since entire GPT NeoX pipeline expects "tokens"
+    tokens, vision_input, multimodal_position_ids, labels, loss_mask, attention_mask, position_ids = _get_batch(
         neox_args, neox_args.tokenizer, keys, data, datatype
     )
-    # print("Tokens shape", tokens.shape)
-    # print("Images shape", images.shape)
-    # print("Multimodal position ids shape", multimodal_position_ids.shape)
-    # print("Labels shape", labels.shape)
-    # print("Loss mask shape", loss_mask.shape)
-    # print("Attention mask shape", attention_mask.shape)
-    # print("Position ids shape", position_ids.shape)
+
     if curr_scheduler is not None:
         # iteration + 1 to align with how/when DeepSpeed updates the buffers
         curriculum_seqlen = curr_scheduler.update_difficulty(neox_args.iteration + 1)
@@ -364,7 +388,7 @@ def get_batch_pipe(data, neox_args, curr_scheduler=None):
             ].contiguous()
 
     # unpack data
-    return (tokens, images, multimodal_position_ids, position_ids, attention_mask), (labels, loss_mask)
+    return (tokens, vision_input, multimodal_position_ids, position_ids, attention_mask), (labels, loss_mask)
 
 
 def forward_step(
@@ -377,14 +401,14 @@ def forward_step(
     # Get the batch.
     if timers is not None:
         timers("batch generator").start()
-    tokens, images, multimodal_position_ids, labels, loss_mask, attention_mask, position_ids = get_batch(
+    tokens, vision_input, multimodal_position_ids, labels, loss_mask, attention_mask, position_ids = get_batch(
         neox_args=neox_args, data_iterator=data_iterator
     )
 
     if timers is not None:
         timers("batch generator").stop()
 
-    outputs = model((tokens, images, multimodal_position_ids, position_ids, attention_mask), neox_args=neox_args)
+    outputs = model((tokens, vision_input, multimodal_position_ids, position_ids, attention_mask), neox_args=neox_args)
     if (
         is_train
         and neox_args.curriculum_learning
@@ -895,7 +919,7 @@ def evaluate(
     forward_step_fn: function with args `neox_args, timers,
                     data_iterator & model that will run a forward pass on the model
     data_iterator: Iterator that iterates over batches of data. Should return data in the form:
-                    {'text': np.array([tokens], dtype=np.int64)}
+                    {'text_input': np.array([tokens], dtype=np.int64)}
                     where the size of the array is the model's context size + 1
                     (`get_batch` transforms it into inputs / labels)
     """

megatron/utils.py
@@ -127,7 +127,7 @@ def get_sequence_ids(tokens, eos_token_id, bos_token_id):
         bos_mode = True
 
     is_separator = torch.eq(tokens,
-                            self.split_token_id)  # type: ignore
+                            split_token_id)  # type: ignore
     cumulative_sep = torch.cumsum(is_separator,
                                     dim=1).to(tokens.dtype)
     # If separator token is bos, we're already done
@@ -138,17 +138,48 @@ def get_sequence_ids(tokens, eos_token_id, bos_token_id):
     left_zeros = cumulative_sep.new_zeros((cumulative_sep.shape[0], 1))
     return torch.cat([left_zeros, cumulative_sep[:, :-1]], dim=1)
     
-def get_shifted_multimodal_position_ids(multimodal_position_ids, vision_seq_length):
+def get_shifted_multimodal_position_ids(input_info):
     
-    pass
+    text_positions = input_info["text"]["positions"]
+    vision_positions = input_info["vision"]["positions"]
+    # audio_positions = input_info["audio"]["positions"]
+
+    vision_seq_length = input_info["vision"]["seq_length"]
+    # audio_seq_length = input_info["audio"]["seq_length"]
+
+    T_B, T_L = text_positions.size()
+    V_B, V_L = vision_positions.size()
+    # A_B, A_L = audio_positions.size()
+    
+    # Concatenate all positions
+    # all_positions = torch.cat((text_positions, vision_positions, audio_positions), dim=1)
+    all_positions = torch.cat((text_positions, vision_positions), dim=1)
+    
+    # Calculate total shifts
+    img_shifts_total = (vision_positions.unsqueeze(1) < all_positions.unsqueeze(-1)).sum(dim=-1) * (vision_seq_length - 1)
+    # aud_shifts_total = (audio_positions.unsqueeze(1) < all_positions.unsqueeze(-1)).sum(dim=-1) * (audio_seq_length - 1)
+    
+    total_shifts = img_shifts_total # TODO + aud_shifts_total
+
+    # Add total shifts to all positions
+    all_positions_shifted = all_positions + total_shifts
+
+    # Split back and update vision_positions and audio_positions
+    shifted_text_positions = all_positions_shifted[:, :T_L]
+    shifted_vision_positions = (all_positions_shifted[:, T_L:T_L+V_L]).repeat_interleave(vision_seq_length, dim=1) + torch.arange(vision_seq_length).repeat(V_B, V_L).type_as(vision_positions)
+    # shited_audio_positions = (all_positions_shifted[:, -A_L:]).repeat_interleave(audio_seq_length, dim=1) + torch.arange(audio_seq_length).repeat(A_B, A_L).type_as(audio_positions)
+    shited_audio_positions = None
+    return shifted_text_positions, shifted_vision_positions, shited_audio_positions
 
 def get_proxy_tokens(position_ids, seq_length, start_ind=100):
     positive_mask = position_ids > 0
-    proxy_tokens = -1*torch.arange(start_ind, start_ind + positive_mask.shape[1]).expand_as(positive_mask) # All vision tokens are given a negative index
+
+    # All vision tokens are given a negative index
+    proxy_tokens = -1*torch.arange(start_ind, start_ind + positive_mask.shape[1]).expand_as(positive_mask).to(positive_mask.device)
     proxy_tokens = torch.repeat_interleave(proxy_tokens, seq_length, dim=1)
     positive_mask = torch.repeat_interleave(positive_mask, seq_length, dim=1)
     proxy_masked_tokens = proxy_tokens*positive_mask
-    proxy_masked_tokens[proxy_masked_tokens == 0] = -1
+    proxy_masked_tokens[proxy_masked_tokens == 0] = 100 
     return proxy_masked_tokens
 
 def get_multimodal_mask(interleaved_tokens):
@@ -162,28 +193,27 @@ def get_multimodal_mask(interleaved_tokens):
     return multimodal_mask
 
 def get_multimodal_attn_mask(
-        text_tokens, 
-        labels,
-        multimodal_position_ids,
-        shifted_multimodal_position_ids, 
+        text_tokens,
+        vision_positions,
+        audio_positions,
+        vision_seq_length,
         input_seq_length,
-        vision_seq_length, 
+        shifted_multimodal_position_ids, 
         eos_token_id,
         bos_token_id,
-        concat_data, 
-        attn_uses_sequence_id, 
+        concat_data,
+        attn_uses_sequence_id,
         device
     ):
     """
     Get multimodal attention mask
     """
-    batch_size = multimodal_position_ids.shape[0]
+    batch_size = text_tokens.shape[0]
     # lower triangular attention mask across all tokens
     mask = torch.tril(torch.ones((1, input_seq_length, input_seq_length), device=device)).expand((batch_size, -1, -1))
 
     # Form vision proxy tokens using shifted multimodal position ids
-    vision_indices = multimodal_position_ids[:, MODALITY_DICT['vision'], :].copy()
-    proxy_vision_tokens = get_proxy_tokens(vision_indices, vision_seq_length)
+    proxy_vision_tokens = get_proxy_tokens(vision_positions, vision_seq_length)
     # Do the same process for Audio #TODO
 
     # Concatenate vision proxy tokens with text tokens 
@@ -193,7 +223,7 @@ def get_multimodal_attn_mask(
     # Equvivalent to : interleaved_tokens = torch.gather(concat_tokens, 1, shifted_multimodal_position_ids)
     
     # Use advanced indexing
-    batch_indices = torch.arange(concat_tokens.size(0))[:, None] 
+    batch_indices = torch.arange(batch_size)[:, None] 
     interleaved_tokens = concat_tokens[batch_indices, shifted_multimodal_position_ids]
 
     
@@ -217,17 +247,15 @@ def get_multimodal_attn_mask(
     
     # convert to binary
     mask = mask.view(
-        1, 1, input_seq_length, input_seq_length
+        batch_size, 1, input_seq_length, input_seq_length
     )
     return mask < 0.5 # TODO Why?
 
 
 def get_multimodal_ltor_masks_and_position_ids(
-    text_tokens,
+    input_info,
     labels,
-    multimodal_position_ids,
     input_seq_length,
-    vision_seq_length,
     eod_token,
     bos_token,
     concat_data=True,
@@ -236,21 +264,27 @@ def get_multimodal_ltor_masks_and_position_ids(
 ):
     """Build masks and position id for left to right model."""
 
-    # Extract batch size and sequence length.
-    batch_size = multimodal_position_ids.shape[0]
+    # Extract batch size and label length
+    batch_size = labels.shape[0]
     label_length = labels.shape[1]
-
-    shifted_multimodal_position_ids = get_shifted_multimodal_position_ids(multimodal_position_ids, vision_seq_length)
-
-    # Attention mask (lower triangular).
+    
+    shifted_text_positions, shifted_vision_positions, shited_audio_positions = get_shifted_multimodal_position_ids(input_info)
+    
+    # Concatenate all of them
+    # Include Audio
+    # shifted_multimodal_position_ids = torch.cat((shifted_text_positions, shifted_vision_positions, shited_audio_positions), dim=1)
+    shifted_multimodal_position_ids = torch.cat((shifted_text_positions, shifted_vision_positions), dim=1)
+    
+    # Attention mask (lower triangular). # TODO: INCLUDE Audio in this
     attention_mask = get_multimodal_attn_mask(
-        text_tokens=text_tokens,
+        text_tokens=input_info["text"]["input"],
+        vision_positions=input_info["vision"]["positions"],
+        audio_positions=None,
+        vision_seq_length=input_info["vision"]["seq_length"],
         input_seq_length=input_seq_length,
-        vision_seq_length=vision_seq_length,
-        multimodal_position_ids=multimodal_position_ids,
         shifted_multimodal_position_ids=shifted_multimodal_position_ids,
-        eos_token=eod_token,
-        bos_token=bos_token,
+        eos_token_id=eod_token,
+        bos_token_id=bos_token,
         concat_data=concat_data,
         attn_uses_sequence_id=attn_uses_sequence_id,
         device=labels.device,
@@ -636,7 +670,7 @@ class CharCounter:
     def __next__(self):
         start = time.time()
         batch = self.data_iterator.__next__()
-        for b in batch["text"]:
+        for b in batch["text_input"]:
             self.token_count += len(b)
             self.char_count += len(self.tokenizer.detokenize(b.tolist()))
         self.batch_count += 1